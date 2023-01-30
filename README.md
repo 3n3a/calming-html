63ebbbe2ab08b5d0d709d7d08d4ce41cdae72361
3n3a <46775561+3n3a@users.noreply.github.com>
2023-01-30 20:05:24 +0100

    refactor code into function + add transform to webp

---
+++ README.md
@@ -0,0 +1,6 @@
+# Calming HTML
+
+## Technologies
+
+* Go Templating Syntax: [Source](https://docs.gomplate.ca/syntax/)
+* Lazy Loading images in Browser: [Source](https://web.dev/browser-level-image-lazy-loading/)
--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"image"
 	"image/jpeg"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
+
+	"golang.org/x/image/webp"
+
+	"github.com/nickalie/go-webpbin"
 )
 
 type ImageTemplateElement struct {
@@ -27,34 +33,16 @@ func main() {
     DIRECTORY := "public/images"
     LAYOUT_NAME := "layout.html"
     OUT_PATH := "public/index.html"
+    IMAGE_WEB_BASE_PATH := "images"
 
-    // Get List of Images in Folder X
-    files, err := ioutil.ReadDir(DIRECTORY)
+    err := tranformAllImagesToWebp(DIRECTORY)
     if err != nil {
         log.Fatal(err)
     }
 
-    var images []ImageTemplateElement
-    for i, file := range files {
-        if !file.IsDir() {
-
-            // Get Image Size
-            i_width, i_height := getImageSize(DIRECTORY, file.Name())
-
-            // Define Loading Priority
-            loadingPriority := "eager"
-            if i+1 > 4 {
-                loadingPriority = "lazy"
-            }
-
-            images = append(images, ImageTemplateElement{
-                Index: i+1,
-                Filename: fmt.Sprintf("images/%s", file.Name()),
-                LoadingPriority: loadingPriority,
-                Width: i_width,
-                Height: i_height,
-            })
-        }
+    images, err := getImagesForSite(DIRECTORY, IMAGE_WEB_BASE_PATH, "webp")
+    if err != nil {
+        log.Fatal(err)
     }
 
     // Fill out Template with List of Images
@@ -78,12 +66,92 @@ func main() {
     
 }
 
+func getImagesForSite(folderPath string, imageBasePathWeb string, targetExtension string) ([]ImageTemplateElement, error) {
+    // Get List of Images in Folder X
+    files, err := ioutil.ReadDir(folderPath)
+    if err != nil {
+        log.Fatal(err)
+    }
 
-func getImageSize(basePath string, imageName string) (int, int) {
+    var images []ImageTemplateElement
+    for i, file := range files {
+        if !file.IsDir() && (getFileExtension(file.Name()) == targetExtension) {            
+            // Get Image Size
+            i_width, i_height := getImageSize(folderPath, file.Name(), targetExtension)
+
+            // Define Loading Priority
+            loadingPriority := "eager"
+            if i+1 > 4 {
+                loadingPriority = "lazy"
+            }
+
+            images = append(images, ImageTemplateElement{
+                Index: i+1,
+                Filename: fmt.Sprintf("%s/%s", imageBasePathWeb, file.Name()),
+                LoadingPriority: loadingPriority,
+                Width: i_width,
+                Height: i_height,
+            })
+        }
+    }
+    return images, nil
+}
+
+func tranformAllImagesToWebp(folderPath string) error {
+    files, err := ioutil.ReadDir(folderPath)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    for _, file := range files {
+        if !file.IsDir() && (getFileExtension(file.Name()) != "webp") {
+            // Transform to webp
+            newName, err := transformToWebp(folderPath, file.Name())
+            if err != nil {
+                log.Fatal(err)
+            }
+            fmt.Println("Transformed", newName)
+        }
+    }
+    return nil
+}
+
+func transformToWebp(basePath string, imageName string) (string, error) {
+    rawName := getFilenameNoExtension(imageName)
+    newName := fmt.Sprintf("%s.webp", rawName)
+
+    err := webpbin.NewCWebP().
+        Quality(60).
+        InputFile(filepath.Join(basePath, imageName)).
+        OutputFile(filepath.Join(basePath, newName)).
+        Run()
+
+    return newName, err
+}
+
+func getFileExtension(fileName string) string {
+    s := strings.Split(fileName, ".")
+    return s[len(s)-1]
+}
+
+func getFilenameNoExtension(fileName string) string {
+    s := strings.Split(fileName, ".")
+    rawName := s[0]
+    return rawName
+}
+
+func getImageSize(basePath string, imageName string, imageType string) (int, int) {
     if reader, err := os.Open(filepath.Join(basePath, imageName)); err == nil {
         defer reader.Close()
-        fmt.Println(reader.Name())
-        image, err := jpeg.DecodeConfig(reader)
+
+        var image image.Config
+        var err error
+        switch imageType {
+        case "jpeg":
+            image, err = jpeg.DecodeConfig(reader)
+        case "webp":
+            image, err = webp.DecodeConfig(reader)
+        }
         if err != nil {
             log.Fatal(err)
         }
